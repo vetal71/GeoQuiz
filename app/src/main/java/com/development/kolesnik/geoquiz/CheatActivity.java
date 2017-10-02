70901ee87e015d030fbb708e2beeb9a2fe696318
vetal71 <vetalkolesnik@gmail.com>
2017-10-02 17:38:19 +0300

    Modified application

--- app/src/main/java/com/development/kolesnik/geoquiz/CheatActivity.java
+++ app/src/main/java/com/development/kolesnik/geoquiz/CheatActivity.java
@@ -1,10 +1,14 @@
 package com.development.kolesnik.geoquiz;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewAnimationUtils;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -14,10 +18,14 @@ public class CheatActivity extends AppCompatActivity {
             "com.development.kolesnik.geoquiz.answer_is_true";
     private static final String EXTRA_ANSWER_SHOWN =
             "com.development.kolesnik.geoquiz.answer_shown";
+    private static final String KEY_ANSWER_SHOWN = "index";
 
     private boolean mAnswerIsTrue;
     private TextView mAnswerTextView;
+    private TextView mAPITextView;
     private Button mShowAnswerButton;
+
+    private boolean mIsAnswerShown;
 
     public static Intent newIntent(Context packageContext, boolean answerIsTrue) {
         Intent intent = new Intent(packageContext, CheatActivity.class);
@@ -34,9 +42,17 @@ public class CheatActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cheat);
 
+        // проверка значения mIsAnswerShown
+        if (savedInstanceState != null) {
+            mIsAnswerShown = savedInstanceState.getBoolean(KEY_ANSWER_SHOWN, false);
+        }
+
         mAnswerIsTrue = getIntent().getBooleanExtra(EXTRA_ANSWER_IS_TRUE, false);
 
         mAnswerTextView = (TextView) findViewById(R.id.answer_text_view);
+
+        mAPITextView = (TextView) findViewById(R.id.build_text_view);
+        mAPITextView.setText("API Level " + String.valueOf(Build.VERSION.SDK_INT));
 
         mShowAnswerButton = (Button) findViewById(R.id.show_answer_button);
         mShowAnswerButton.setOnClickListener(new View.OnClickListener() {
@@ -47,9 +63,34 @@ public class CheatActivity extends AppCompatActivity {
                 } else {
                     mAnswerTextView.setText(R.string.false_button);
                 }
-                setAnswerShownResult(true);
+                mIsAnswerShown = true;
+                setAnswerShownResult(mIsAnswerShown);
+
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                    int cx = mShowAnswerButton.getWidth() / 2;
+                    int cy = mShowAnswerButton.getHeight() / 2;
+                    float radius = mShowAnswerButton.getWidth();
+                    Animator anim = ViewAnimationUtils
+                            .createCircularReveal(mShowAnswerButton, cx, cy, radius, 0);
+                    anim.addListener(new AnimatorListenerAdapter() {
+                                         @Override
+                                         public void onAnimationEnd(Animator animation) {
+                                             super.onAnimationEnd(animation);
+                                             mShowAnswerButton.setVisibility(View.INVISIBLE);
+                                         }
+                                     });
+                    anim.start();
+                } else {
+                    mShowAnswerButton.setVisibility(View.INVISIBLE);
+                }
             }
         });
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putBoolean(EXTRA_ANSWER_SHOWN, mIsAnswerShown);
     }
 
     private void setAnswerShownResult(boolean isAnswerShown) {
